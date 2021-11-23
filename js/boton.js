b55b0ff31e085d5e64af287ad3d9a8de0c783429
axelnub <89424797+axelnub@users.noreply.github.com>
2021-11-23 00:00:04 -0300

    Add files via upload

--- js/boton.js
+++ js/boton.js
@@ -1,4 +1,5 @@
 function nocturno() {document.body.style.backgroundColor = "black"; document.body.style.color = "white"; }
 
+
 function botoncre() {
   document.getElementById("creador").innerHTML = "Axel Abita";}
